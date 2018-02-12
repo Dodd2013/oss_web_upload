efef3a59d1c2abf96230db3ac869f758d4bc66b8
dodd <291106637@qq.com>
2018-02-12 20:09:18 +0800

    update config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	oss: {
-		id: '6MKOqxGiGU4AUk44',
-		key: 'ufu7nS8kS59awNihtjSonMETLI0KLy',
-		host: 'http://post-test.oss-cn-hangzhou.aliyuncs.com',
+		id: 'LTAIaCrvj8H26fJg',
+		key: 'DwP1FSgpDcVtXH9KaUYgkzbnnU9Kck',
+		host: 'oss-cn-beijing.aliyuncs.com',
 		callbackUrl: 'http://oss-demo.aliyuncs.com:23450',
 		expire: '10' //second
 	}
