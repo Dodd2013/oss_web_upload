8284677a576e25d1d34164dfc2f2a10d373553ad
dodd <291106637@qq.com>
2018-02-12 20:27:17 +0800

    update oss host

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	oss: {
 		id: 'LTAIaCrvj8H26fJg',
 		key: 'DwP1FSgpDcVtXH9KaUYgkzbnnU9Kck',
-		host: 'http://oss-cn-beijing.aliyuncs.com',
+		host: 'http://test-for-web-upload.oss-cn-beijing.aliyuncs.com',
 		callbackUrl: 'http://oss-demo.aliyuncs.com:23450',
 		expire: '10' //second
 	}
